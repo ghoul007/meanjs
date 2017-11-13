fe24d4bf1122d02a63fa76b7400e0188d033a2b8
ahmed <ghoul.ahmed5@gmail.com>
2017-11-13 10:15:12 +0100

    add axios + first service in api.js

--- server/routes/angular.js
+++ server/routes/angular.js
@@ -1,17 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var path = require('path');
+var path = require("path");
 
-var angularPath = path.resolve(__dirname, '../../dist')
-
+var angularPath = path.resolve(__dirname, "../../dist");
 
 router.use(express.static(angularPath));
 
 /* GET home page. */
-router.get('*', function(req, res, next) {
-    if (req.url.startsWith('/api')) return next();
+router.get("*", function(req, res, next) {
+    if (req.url.startsWith("/api")) return next();
 
-    res.sendFile(path.join(angularPath, 'index.html'));
+    res.sendFile(path.join(angularPath, "index.html"));
 });
 
 module.exports = router;
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,16 +1,28 @@
 var express = require("express");
 var router = express.Router();
+var axios = require("axios");
+var url = "https://jsonplaceholder.typicode.com";
 
 /* GET users listing. */
 router.get("/", function(req, res, next) {
-    res.send("respond with a resource");
+    res.send("api ...");
 });
 
-router.get("/:param", function(req, res, next) {
-    var params = req.params;
-    var query = req.query;
-    Object.assign(params, query);
-    res.json(params);
+router.get("/posts", function(req, res, next) {
+    return axios.get(`${url}/posts`)
+        .then(function(data) {
+            res.status(200).json(data.data);
+        })
+        .catch(function(err) {
+            res.status(500).send(err);
+        });
 });
+
+// router.get("/:param", function(req, res, next) {
+//     var params = req.params;
+//     var query = req.query;
+//     Object.assign(params, query);
+//     res.json(params);
+// });
 
 module.exports = router;
