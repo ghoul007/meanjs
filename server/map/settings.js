f2baee0845b08e94c613976bf696f7a60817d993
Ahmed <ghoul.ahmed5@gmail.com>
2017-12-14 00:54:58 +0100

    update

--- server/map/settings.js
+++ server/map/settings.js
@@ -5,8 +5,8 @@ module.exports = {
                 "login": { "method": "POST", "url": "/api/movies/login" },
                 "change": { "method": "POST", "url": "/api/movies/change" },
                 "list": { "method": "GET", "url": "/api/movies" },
-                "create": { "method": "PUT", "url": "/api/movies" },
-                "update": { "method": "POST", "url": "/api/movies/<uuid>" },
+                "create": { "method": "POST", "url": "/api/movies" },
+                "update": { "method": "PUT", "url": "/api/movies/<uuid>" },
                 "delete": { "method": "DELETE", "url": "/api/movies/<uuid>" },
                 "exist": { "method": "POST", "url": "/api/movies/exist" },
                 "search": { "method": "POST", "url": "/api/movies/search" }
